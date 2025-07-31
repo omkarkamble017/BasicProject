0e93bdde40e0a3a8fd7efa0327989fbecb0a5c0f
komka <komka@OMKAR>
2025-07-31 10:15:15 +0530

    Add project files.

---
+++ BasicProject/Program.cs
@@ -0,0 +1,58 @@
+﻿class Person
+{
+    public int id;
+    public string name;
+    public int age;
+
+    public void Display()
+    {
+        Console.WriteLine($"[ eId : {this.id}  ename : {this.name}  eAge : {this.age}]");
+    }
+
+    public bool IsLegalAge()
+    {
+        return age >= 18;
+    }
+
+    public void ChangeName(string name)
+    {
+        this.name = name;
+    }
+
+    public void GrowOlder() => this.age++;
+
+
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Person P1 = new Person()
+        {
+            name = "omkar",
+            id = 1,
+            age = 23
+
+        };
+        Person P2 = new Person()
+        {
+            name = "Om",
+            id = 2,
+            age = 17
+        };
+
+        P1.Display();
+        Console.WriteLine($"Is {P1.name} legal age? {P1.IsLegalAge()}");
+        P1.GrowOlder();
+        P1.ChangeName("Omkar Kamble");
+        P1.Display();
+        P1.ChangeName("Omkar");
+
+        P2.Display();
+        P2.ChangeName("ajinkya");
+        P2.GrowOlder();
+        P2.Display();
+
+    }
+}
